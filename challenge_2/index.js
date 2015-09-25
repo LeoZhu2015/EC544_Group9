4501e5a1e01a7553e185613e2231de51aa350441
Qingqing Li <qqli@bu.edu>
2015-09-25 15:27:16 -0400

    add insertions to mongodb

--- challenge_2/index.js
+++ challenge_2/index.js
@@ -8,6 +8,10 @@ portConfig = {
 	baudRate: 9600,
 	parser: SerialPort.parsers.readline("\n")
 };
+var MongoClient = require('mongodb').MongoClient;
+var assert = require('assert');
+var ObjectId = require('mongodb').ObjectID;
+var url = 'mongodb://localhost:27017/test';
 
 var sp;
 
@@ -28,25 +32,54 @@ http.listen(3000, function(){
 });
 
 
-	sp.on('data', function(data) {
-		console.log(data);
-			message = data.split(',');
-			// console.log(message[0]);
-			if (message[0] === 'A'){
-				data = message[1];
-				console.log(data);
-				io.emit("A",data);
-			}
-			if (message[0] === 'B'){
-				data = message[1];
-				io.emit("B", data);
-			}
-			if (message[0] === 'C'){
-				data = message[1];
-				io.emit("C", data);
-			}
-			if (message[0] === 'D'){
-				data = message[1];
-				io.emit("D", data);
-			}
+sp.on('data', function(data) {
+	//console.log(data);
+		message = data.split(',');
+		// console.log(message[0]);
+		if (message[0] === 'A'){
+			data = message[1];
+			//console.log(data);
+			io.emit("A",data);
+		}
+		if (message[0] === 'B'){
+			data = message[1];
+			io.emit("B", data);
+		}
+		if (message[0] === 'C'){
+			data = message[1];
+			io.emit("C", data);
+		}
+		if (message[0] === 'D'){
+			data = message[1];
+			io.emit("D", data);
+		}
+
+var sensor_data = {
+     "sensor_name" : message[0],
+      "temperature" : message[1],
+      "unit":"C"
+   }
+ console.log(sensor_data);
+
+ 
+var insertDocument = function(db, callback) {
+   db.collection('sensor').insertOne(sensor_data, function(err, result) {
+    assert.equal(err, null);
+    console.log("Inserted a document into the restaurants collection.");
+    callback(result);
+  });
+};
+
+MongoClient.connect(url, function(err, db) {
+  assert.equal(null, err);
+  console.log("Connected correctly to server.");
+  insertDocument(db, function() {
+    db.close();
+  });
 });
+
+});
+
+
+
+
