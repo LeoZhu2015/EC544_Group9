be6792a867b0e30fd552abb9055c38688d644d50
liang-zhao <lzhaoee@bu.edu>
2015-10-25 14:44:32 -0400

    algorithm test

---
+++ challenge_4/test.ino
@@ -0,0 +1,124 @@
+#include <Servo.h>
+#include <I2C.h>
+
+#define    LIDARLite_ADDRESS   0x62          // Default I2C Address of LIDAR-Lite.
+#define    RegisterMeasure     0x00          // Register to write to initiate ranging.
+#define    MeasureValue        0x04          // Value to initiate ranging.
+#define    RegisterHighLowB    0x8f          // Register to get both High and Low bytes in 1 call.
+
+Servo wheels; // servo for turning the wheels
+Servo esc; // not actually a servo, but controlled like one!
+bool startup = true; // used to ensure startup only happens once
+int startupDelay = 1000; // time to pause at each calibration step
+double maxSpeedOffset = 45; // maximum speed magnitude, in servo 'degrees'
+double maxWheelOffset = 85; // maximum wheel turn magnitude, in servo 'degrees'
+
+double carDistance = get_LIDAR_data();
+double maxDistance = 80;
+double stepPoint = 0;
+double carAngle = 0;
+double maxAngle = 90;
+double distanceError = carDistance - stepPoint;
+double angleError = carAngle - disredAngle;
+
+
+int get_LIDAR_data(){
+    // Write 0x04 to register 0x00
+  uint8_t nackack = 100; // Setup variable to hold ACK/NACK resopnses     
+  while (nackack != 0){ // While NACK keep going (i.e. continue polling until sucess message (ACK) is received )
+    nackack = I2c.write(LIDARLite_ADDRESS,RegisterMeasure, MeasureValue); // Write 0x04 to 0x00
+    delay(1); // Wait 1 ms to prevent overpolling
+  }
+
+  byte distanceArray[2]; // array to store distance bytes from read function
+  
+  // Read 2byte distance from register 0x8f
+  nackack = 100; // Setup variable to hold ACK/NACK resopnses     
+  while (nackack != 0){ // While NACK keep going (i.e. continue polling until sucess message (ACK) is received )
+    nackack = I2c.read(LIDARLite_ADDRESS,RegisterHighLowB, 2, distanceArray); // Read 2 Bytes from LIDAR-Lite Address and store in array
+    delay(300); // Wait 1 ms to prevent overpolling
+  }
+  int distance = (distanceArray[0] << 8) + distanceArray[1];  // Shift high byte [0] 8 to the left and add low byte [1] to create 16-bit int
+  
+  // Print Distance
+  Serial.println(distance);
+  return distance;
+}
+ 
+void setup()
+{
+  wheels.attach(8); // initialize wheel servo to Digital IO Pin #8
+  esc.attach(9); // initialize ESC to Digital IO Pin #9
+  /*  If you're re-uploading code via USB while leaving the ESC powered on, 
+   *  you don't need to re-calibrate each time, and you can comment this part out.
+   */
+  calibrateESC();
+}
+
+/* Convert degree value to radians */
+double degToRad(double degrees){
+  return (degrees * 71) / 4068;
+}
+
+/* Convert radian value to degrees */
+double radToDeg(double radians){
+  return (radians * 4068) / 71;
+}
+
+/* Calibrate the ESC by sending a high signal, then a low, then middle.*/
+void calibrateESC(){
+    esc.write(180); // full backwards
+    delay(startupDelay);
+    esc.write(0); // full forwards
+    delay(startupDelay);
+    esc.write(90); // neutral
+    delay(startupDelay);
+    esc.write(90); // reset the ESC to neutral (non-moving) value
+}
+
+/* Oscillate between various servo/ESC states, using a sine wave to gradually 
+ *  change speed and turn values.
+ */
+void oscillate(){
+  for (int i =0; i < 360; i++){
+    double rad = degToRad(i);
+    double speedOffset = sin(rad) * maxSpeedOffset;
+    double wheelOffset = sin(rad) * maxWheelOffset;
+    esc.write(90 + speedOffset);
+    wheels.write(90 + wheelOffset);
+    delay(50);
+  }
+}
+ 
+/*void loop()
+{
+   oscillate();
+}
+*/
+
+
+void test(){
+if (distanceError > maxDistance){
+  distanceError = maxDistance;
+}
+
+double desiredAngle = distanceError/maxDistance * maxAngle;
+
+if (angelError > maxAngel){
+  angelError = maxAngel;
+}
+
+double wheelAngel = angelError/maxAngel * 180; //don't know whether it's 180
+
+wheels.write(wheelAngel);
+
+}
+
+
+
+
+
+
+
+
+
