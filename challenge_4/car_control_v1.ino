01c72c4a1d5d5a8b3d1807b473f4f70ca73e3aeb
Yuchen Liu <lyc@bu.edu>
2015-10-29 11:33:14 -0400

    merge version of our codes

---
+++ challenge_4/car_control_v1.ino
@@ -0,0 +1,403 @@
+/* 
+http://pulsedlight3d.com
+
+This sketch demonstrates getting distance from multiple LIDAR-Lite sensors by enabling the Power Enable line of the sensor 
+to read from, and disabling the others.
+
+It utilizes the 'Arduino I2C Master Library' from DSS Circuits:
+http://www.dsscircuits.com/index.php/articles/66-arduino-i2c-master-library 
+
+You can find more information about installing libraries here:
+http://arduino.cc/en/Guide/Libraries
+*/
+#include <Servo.h>
+#include <I2C.h>
+#include <math.h>
+#include <PID_v1.h>
+#include<SoftwareSerial.h>
+
+#define    LIDARLite_ADDRESS     0x62          // Default I2C Address of LIDAR-Lite.
+#define    RegisterMeasure       0x00          // Register to write to initiate ranging.
+#define    MeasureValue          0x04          // Value to initiate ranging.
+#define    RegisterHighLowB      0x8f          // Register to get both High and Low bytes in 1 call.
+#define     THETA                60  //max degree
+#define    Setpoint              80 
+
+SoftwareSerial XBee(2,3); //RX,TX
+Servo wheels; // servo for turning the wheels
+Servo esc; // not actually a servo, but controlled like one!
+bool startup = true; // used to ensure startup only happens once
+int startupDelay = 1000; // time to pause at each calibration step
+double maxSpeedOffset = 45; // maximum speed magnitude, in servo 'degrees'
+double maxWheelOffset = 85; // maximum wheel turn magnitude, in servo 'degrees'
+int sensorPins[] = {4,6}; // Array of pins connected to the sensor Power Enable lines
+int sensorPinsArraySize = 2; // The length of the array
+int distanceA;
+int distanceB;
+//Define Variables we'll be connecting to
+//double Setpoint, Input, Output;
+
+//Define the aggressive and conservative Tuning Parameters
+double aggKp=4, aggKi=0.2, aggKd=1;
+double consKp=1, consKi=0, consKd=0.25;
+
+//Motion Detect
+int sensorPin = A0;    // select the input pin for the potentiometer
+int sensorValue = 0;  // variable to store the value coming from the sensor
+volatile byte revolutions;
+unsigned int rpmilli;
+float speed;
+unsigned long timeold;
+
+//Specify the links and initial tuning parameters
+//PID myPID(&Input, &Output, &Setpoint, consKp, consKi, consKd, DIRECT);
+void setup(){
+  XBee.begin(9600);
+  Serial.begin(9600); //Opens serial connection at 115200bps.     
+  I2c.begin(); // Opens & joins the irc bus as master
+  delay(100); // Waits to make sure everything is powered up before sending or receiving data  
+  I2c.timeOut(50); // Sets a timeout to ensure no locking up of sketch if I2C communication fails
+  
+  for (int i = 0; i < sensorPinsArraySize; i++){
+    pinMode(sensorPins[i], OUTPUT); // Pin to first LIDAR-Lite Power Enable line
+//    Serial.print(sensorPins[i]);
+  }
+  
+  wheels.attach(8); // initialize wheel servo to Digital IO Pin #8
+  esc.attach(9); // initialize ESC to Digital IO Pin #9
+  /*  If you're re-uploading code via USB while leaving the ESC powered on, 
+   *  you don't need to re-calibrate each time, and you can comment this part out.
+   */
+  calibrateESC();
+  //myPID.SetMode(AUTOMATIC);
+
+  //Motion Detect
+  revolutions = 0;
+  rpmilli = 0;
+  timeold = 0;
+}
+/* Convert degree value to radians */
+double degToRad(double degrees){
+  return (degrees * 71) / 4068;
+}
+
+
+/* Convert radian value to degrees */
+double radToDeg(double radians){
+  return (radians * 4068) / 71;
+}
+
+/* Calibrate the ESC by sending a high signal, then a low, then middle.*/
+void calibrateESC(){
+    esc.write(180); // full backwards
+    delay(startupDelay);
+    esc.write(0); // full forwards
+    delay(startupDelay);
+    esc.write(90); // neutral
+    delay(startupDelay);
+    esc.write(90); // reset the ESC to neutral (non-moving) value
+}
+
+void loop(){
+char command;
+command = XBee.read();
+if(command=='w'){
+  esc.write(90);
+  esc.write(70);
+  delay(2000);
+}
+else if(command=='s'){
+  esc.write(90);
+    delay(2000);
+}
+else if(command=='a'){
+  wheels.write(135);
+  esc.write(90);
+  esc.write(70);
+    delay(2000);
+}
+else if(command=='d'){
+  wheels.write(45);
+  esc.write(90);
+  esc.write(70);
+    delay(2000);
+}
+else if(command=='z'){
+  wheels.write(135);
+  esc.write(90);
+  esc.write(110);
+    delay(2000);
+}
+else if(command=='c'){
+  wheels.write(45);
+  esc.write(90);
+  esc.write(110);
+    delay(2000);
+}
+else if(command=='x'){
+  esc.write(90);
+    delay(2000);
+}
+else{
+ if(getDistance(2)>40){
+   enableDisableSensor(4); // Turn on sensor attached to pin 1 and disable all others
+ //  Serial.print("SensorA ");
+ //  Serial.print(readDistance()); // Read Distance from Sensor
+ //  Serial.println(""); // Print "." to separate readings
+   distanceA=readDistance();
+  
+   enableDisableSensor(6); //Turn on sensor attached to pin 2 and disable all others
+ //  Serial.print("SensorB ");
+ //  Serial.print(readDistance()); // Read Distance from Sensor
+ //  Serial.println(""); // Print "." to separate readings
+   distanceB=readDistance();
+  
+ 
+    D_CAR((double)distanceA,(double)distanceB);
+    esc.write(70); // full forwards
+   } 
+    else if(abs(distanceA-distanceB)>20){
+      esc.write(100);
+      delay(1000);
+     }
+     else{
+       esc.write(90);
+     }
+} 
+//Motion Detect
+   if(disk_output() == 0)
+   {
+    revolutions++;
+    Serial.print("revolutions");
+     Serial.println(revolutions);
+        if(revolutions >= 10)
+          {
+          rpm_fun(); 
+          }
+   } 
+}
+
+
+double D_CAR(double a, double b){
+  double w = 13.0; //width of the car
+  double l = 27.0; //length of the car
+  double c = ((a+b+w)/2);
+  double __d = (l/(a-b));
+  double __f = atan(__d);
+  double g = sin(__f);
+  double e = c*g; //distance to the wall from center of the car
+  double gap1 = Setpoint-a;
+  double gap2 = Setpoint-b;
+  double gap = Setpoint -abs(e);
+  double theta;
+ //  Input = a;
+  // myPID.SetTunings(consKp, consKi, consKd);
+  // myPID.Compute();
+  //Serial.println(Output);
+  //Serial.println(__f);
+  Serial.println(a);
+  Serial.println(b);
+  Serial.println();
+
+//  if(a>200||b>200){
+//    
+//  }
+  if(abs(a-b)>20&&abs(a-b)<150){
+    //Angle first
+        if (__f < 0){  // The car is going right
+    double orientation =  (180 + radToDeg(__f)-5);
+    //Serial.print("Orientation : ");
+    //Serial.println(orientation);
+    double wheelwrite =  (orientation) ;
+//    Serial.print("wheelwrite : ");
+//    Serial.print(wheelwrite);
+    wheels.write(wheelwrite) ;
+    Serial.println("b");
+    }
+
+    if( __f > 0){
+      double orientation = (radToDeg(__f)-5);
+//      Serial.print("Orientation : ");
+//      Serial.println(orientation); 
+      double wheelwrite = (orientation);
+//      Serial.print("wheelwrite : ");
+//      Serial.print(wheelwrite);
+      wheels.write(wheelwrite) ; 
+      Serial.println("c");
+      }
+  }
+  else if(abs(a-b)<20){ //distance first
+   if (abs(gap)<5||abs(gap)<5){
+    wheels.write(90);
+    Serial.println("I'm goint straight");
+  }
+  else if(gap>0){
+    if(abs(gap)>20){
+      theta = THETA;
+    }else{
+      theta = (gap/20)*THETA;
+    } 
+    wheels.write(90-theta);
+    Serial.println("I am going right");
+  }
+  else if(gap<0){
+    if(abs(gap)>20){
+      theta = THETA;
+    }else{
+      theta = (abs(gap)/20)*THETA;
+    } 
+    wheels.write(90+theta);
+    Serial.println("I am going left");
+  }
+  }
+else{
+  wheels.write(90);
+}
+  
+//  if (abs(e)<5||abs(e)<5){
+//    wheels.write(90);
+//    Serial.println("I'm goint straight");
+//  }
+//  else if(gap1>0&&abs(gap1)>15){//Let the car go right
+//    if(Output>50){
+//      wheels.write(45);
+//    }
+//    else{
+//       wheels.write(90-Output);
+//    }
+//      Serial.println("I'm goint right");
+//   }
+//  else if(gap1<0&&abs(gap1)>15){
+//    if(Output>50){
+//      wheels.write(135);
+//    }else{
+//      wheels.write(90+Output);
+//    }
+//      Serial.println("I'm goint left");
+//   }
+//   else{
+//    if(abs(b-a)<5){
+//      wheels.write(90);
+//      Serial.println("a");
+//    }
+//    if (__f < 0){  // The car is going right
+//    double orientation =  (180 + radToDeg(__f)-5);
+//    //Serial.print("Orientation : ");
+//    //Serial.println(orientation);
+//    double wheelwrite =  (orientation) ;
+////    Serial.print("wheelwrite : ");
+////    Serial.print(wheelwrite);
+//    wheels.write(wheelwrite) ;
+//    Serial.println("b");
+//    }
+//
+//    if( __f > 0){
+//      double orientation = (radToDeg(__f)-5);
+////      Serial.print("Orientation : ");
+////      Serial.println(orientation); 
+//      double wheelwrite = (orientation);
+////      Serial.print("wheelwrite : ");
+////      Serial.print(wheelwrite);
+//      wheels.write(wheelwrite) ; 
+//      Serial.println("c");
+//      }
+//    }
+}
+
+
+void enableDisableSensor(int sensorPin){
+  for (int i = 0; i < sensorPinsArraySize; i++){
+      digitalWrite(sensorPins[i], LOW); // Turn off all sensors
+  }
+  digitalWrite(sensorPin, HIGH); // Turn on the selected sensor
+  delay(1); // The sensor takes 1msec to wake
+}
+
+int readDistance(){
+  uint8_t nackack = 100; // Setup variable to hold ACK/NACK resopnses     
+  while (nackack != 0){ // While NACK keep going (i.e. continue polling until sucess message (ACK) is received )
+    nackack = I2c.write(LIDARLite_ADDRESS,RegisterMeasure, MeasureValue); // Write 0x04 to 0x00
+    delay(1); // Wait 1 ms to prevent overpolling
+  }
+
+  byte distanceArray[2]; // array to store distance bytes from read function
+  
+  // Read 2byte distance from register 0x8f
+  nackack = 100; // Setup variable to hold ACK/NACK resopnses     
+  while (nackack != 0){ // While NACK keep going (i.e. continue polling until sucess message (ACK) is received )
+    nackack = I2c.read(LIDARLite_ADDRESS,RegisterHighLowB, 2, distanceArray); // Read 2 Bytes from LIDAR-Lite Address and store in array
+    delay(1); // Wait 1 ms to prevent overpolling
+  }
+  int distance = (distanceArray[0] << 8) + distanceArray[1];  // Shift high byte [0] 8 to the left and add low byte [1] to create 16-bit int
+  
+  return distance;   // Print Sensor Name & Distance
+   
+}
+
+double getDistance(int pinNum){
+  double d;
+  int RawADC = analogRead(pinNum);
+  double RawADC_do = (double)RawADC;
+  double Raw = (5.0/1023)*RawADC; 
+//  Serial.print("Input: ");
+//  Serial.print(Raw);
+//  Serial.println(" V");
+  if(Raw<1.25){
+  //   double distance = exp(8.5841-log(RawADC_do));
+  //   double distance_cm= 2.54*distance;     
+ //    return distance_cm;
+    d = 509.6*exp((-4.652)*Raw)+118.6*exp((-0.7075)*Raw);
+    return d; 
+  }
+  else if(1.25<=Raw<=2.0){
+    d = (Raw-0.25)/50; 
+    return (1.0/d); 
+  }
+  else if(2.0<Raw<2.52){
+    d = (Raw-0.99053)/30.59; 
+    return (1.0/d);  
+  }
+  else if(Raw>=2.52){
+    d = (Raw-1.7555)/15.29;
+    return ((1.0/d)); 
+  } 
+}
+
+void rpm_fun(){
+
+    //Update RPM every 20 counts, increase this for better RPM resolution,
+    //decrease for faster update
+    double duration = (millis() - timeold)/1000 ;
+    
+    rpmilli = (60 /duration) * revolutions ;
+      Serial.print("duration");
+     Serial.println(duration);
+    timeold = millis();
+    revolutions = 0;
+    
+    Serial.print(" speed:");
+    Serial.print(rpmilli * 0.026);
+    Serial.println(" kmph");
+    String message_speed = String(rpmilli*0.026);
+    String mymessage = String(message_speed+" kmph");
+    XBee.print(mymessage); 
+}
+
+int disk_output(){
+ // read the value from the sensor:
+  sensorValue = analogRead(sensorPin);    
+  
+  
+  if(sensorValue > 600 ){
+    return 1;
+  }
+  else{
+    return 0;
+  }
+//Serial.print ("Opto returning: "); 
+//Serial.println(sensorValue);
+delay(100);
+
+}
+
+
